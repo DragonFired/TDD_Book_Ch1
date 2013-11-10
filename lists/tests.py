2ccbcb39b4bdafec4fe61ab6121a4cce46174c00
DragonFired <aranafireheart@mac.com>
2013-11-10 12:47:42 -0500

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,8 @@ Replace this with more appropriate tests for your application.
 
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
